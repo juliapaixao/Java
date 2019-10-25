b11d2a5968b555aea4b96e1288c0e9ef20629638
JuliaPaixao <julia.paixao@bndes.gov.br>
2019-10-25 17:18:27 -0300

    Leitura de arquivo

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,34 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+
+public class TesteLeitura {
+
+    public static void main(String[] args) throws IOException {
+
+        //Fluxo de entrada com arquivo (em binário)
+        FileInputStream fis = new FileInputStream("vasco.txt");
+        //Transforma bits e bytes em caracter
+        InputStreamReader isr = new InputStreamReader(fis);
+        //Lê as linhas inteiras do arquivo de texto
+        BufferedReader br = new BufferedReader(isr);
+
+
+        //realiza leitura de linha por linha
+        String linha = br.readLine();
+        System.out.println(linha);
+
+
+        //lê cada linha do arquivo
+        while(linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+
+        //Fecha a leitura do bufferedReader
+        br.close();
+
+    }
+}
